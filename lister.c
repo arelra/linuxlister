6623ffe05e426ac5289b098fc3516aa7e9170a28
Ravi Lal <ravilal7@gmail.com>
2015-04-09 14:30:57 +0100

    gpl

--- lister.c
+++ lister.c
@@ -6,8 +6,27 @@ Source: http://man7.org/linux/man-pages/man2/getdents.2.html
 
 compile: gcc lister.c -o lister
 
-usage: ./lister [-l | -c] dir
-e.g to list to stdout all flies:  ./lister -l /mydir, to count files: ./lister -c /mydir
+usage:
+./lister [-l | -c] dir
+
+to list to stdout all flies:
+./lister -l /mydir
+
+to count files:
+./lister -c /mydir
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
